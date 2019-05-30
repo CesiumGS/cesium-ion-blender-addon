d7233d2ae3a7c4e1c64f0739ef903ae9d768e339
Brandon <contact@brandonbarker.me>
2019-05-30 15:47:52 -0400

    Disable local development & add external parameters

--- globals.py
+++ globals.py
@@ -1,12 +1,14 @@
 APP_CATEGORY = "Cesium ion"
 APP_OPERATOR_PREFIX = "csm"
 APP_PACKAGE = __package__
-LOCAL = True
+LOCAL = False
 if LOCAL:
     CLIENT_ID = "4"
     ION_ADDRESS = "http://composer.test:8081"
     API_ADDRESS = "http://api.composer.test:8081"
 else:
-    pass  # TODO -- ADD INFO
+    CLIENT_ID = "19"
+    ION_ADDRESS = "https://cesium.com/ion"
+    API_ADDRESS = "https://api.cesium.com"
 REDIRECT_ADDRESS = "localhost"
 REDIRECT_PORT = 10101
