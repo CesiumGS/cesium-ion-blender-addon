227bdfdf10714e65829e3313dbf5c8f06309b7ad
“Brandon <contact@brandonbarker.me>
2019-06-25 08:54:40 -0400

    Missing Third Party, Version Compare Util Bug, Bumped Version
    The library six was missing which was causing a silent failure when exporting
    
    Untested version comparison code was not executed until now, leaving bugs that needed to fix. Comparing tuples instead of lists
    
    Bumped version to show code change

--- io-cesium-ion/__init__.py
+++ io-cesium-ion/__init__.py
@@ -1,14 +1,12 @@
 bl_info = {
     "name": "Cesium ion",
-    "description":
-    "Upload and tile models with Cesium ion. https://cesium.com",
+    "description": "Upload and tile models with Cesium ion. https://cesium.com",
     "author": "Analytical Graphics, Inc.",
-    "wiki_url":
-    "https://github.com/AnalyticalGraphicsInc/ion-blender-exporter",
-    "version": (1, 0),
+    "wiki_url": "https://github.com/AnalyticalGraphicsInc/ion-blender-exporter",
+    "version": (1, 1),
     "blender": (2, 80, 0),
     "location": "View3D > Cesium ion",
-    "category": "Import-Export"
+    "category": "Import-Export",
 }
 
 import bpy
@@ -20,21 +18,26 @@ from .panels import *
 from .cache import load_properties
 
 __classes__ = [
-    UserProperties, UserPreferences, ExportProperties, ExportPanel,
-    OAuthOperator, GetTokenOperator, ClearTokenOperator, ExportUploadOperator,
-    ProgressOperator, ProgressProperty
+    UserProperties,
+    UserPreferences,
+    ExportProperties,
+    ExportPanel,
+    OAuthOperator,
+    GetTokenOperator,
+    ClearTokenOperator,
+    ExportUploadOperator,
+    ProgressOperator,
+    ProgressProperty,
 ]
 
-register_classes, unregister_classes = bpy.utils\
-    .register_classes_factory(__classes__)
+register_classes, unregister_classes = bpy.utils.register_classes_factory(__classes__)
 
 
 def register():
     register_classes()
     bpy.types.WindowManager.csm_user = PointerProperty(type=UserProperties)
     bpy.types.Scene.csm_export = PointerProperty(type=ExportProperties)
-    bpy.types.WindowManager.csm_progress = PointerProperty(
-        type=ProgressProperty)
+    bpy.types.WindowManager.csm_progress = PointerProperty(type=ProgressProperty)
 
     # Update login persistence
     @persistent
